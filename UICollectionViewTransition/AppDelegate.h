6c880f0ecf65e471accd737355dfb9e7a0d937b8
[sunfengqian] <[sunfengqian163@163.com]>
2016-03-24 09:31:12 +0800

    Initial Commit

---
+++ UICollectionViewTransition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UICollectionViewTransition
+//
+//  Created by hahajing on 16/3/24.
+//  Copyright © 2016年 hahajing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UICollectionViewTransition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UICollectionViewTransition
+//
+//  Created by hahajing on 16/3/24.
+//  Copyright © 2016年 hahajing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
